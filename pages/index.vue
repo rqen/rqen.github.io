625263815a535de2c87aab9a3f98297374300f96
Christian Røen Hansen <rqen1337@gmail.com>
2024-02-24 20:44:48 +0100

    Some intro and adjustments

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <script setup>
 import gsap from 'gsap'
+import { onMounted } from 'vue'
 
 const colors = ['#fff', '#fdfdfd', '#ededed', '#bada55']
 const checkersBgColor = ref('')
+
+const doIntro = ref(false)
 
 const findRandomColor = ()=>{
   let randomColor = gsap.utils.random(colors)
@@ -16,13 +19,30 @@ const setBg = ()=>{
   checkersBgColor.value = findRandomColor()
   console.log("🚀 ~ setBg ~ checkersBgColor.value:", checkersBgColor.value)
 }
+
+onMounted(()=>{
+  setTimeout(()=>{
+    doIntro.value = true
+  }, 500)
+})
 </script>
 
 <template>
   <div @click="setBg">
-    <div class="content">
-      <div class="outbound">
-        <ul>
+    <div class="content" :class="{'-intro': !doIntro}">
+      <div class="title">
+        <div class="upper">
+          <div>RQEN</div>
+          <div>.COM</div>
+        </div>
+        <div class="spacer"></div>
+        <div class="lower">
+          <div>CHRISTIAN</div>
+          <div>RØEN</div>
+        </div>
+      </div>
+      <div class="outbound page-margin">
+        <ul class="outbound-list">
           <li>
             <a href="https://www.linkedin.com/in/christian-r%C3%B8en-hansen-121b696" title="LinkedIn">LinkedIn</a>
           </li>
@@ -35,12 +55,12 @@ const setBg = ()=>{
             <a href="https://github.com/rqen" title="Github">Github</a>
           </li>
           <li>
-            <a href="https://twitter.com/rqen" title="X or whatever it currently called">Twitter</a>
+            <a href="https://twitter.com/rqen" title="X or whatever it's currently called">Twitter</a>
           </li>
         </ul>
       </div>
 
-      <div class="projects mt-4">
+      <div class="projects page-margin mt-4">
         <h2 class="tagline">A few brands i've helped over the years</h2>
         <ul class="projects-list">
           <li class="project">
@@ -61,7 +81,7 @@ const setBg = ()=>{
           </li>
           <li class="project">
             <!-- <h3 class="heading">Pandora</h3> -->
-            <a href="#nolongeronline" class="link">Pandora, product launch sites</a>
+            <a href="#nolongeronline" class="no-link">Pandora - product launch sites</a>
           </li>
           <li class="project">
             <!-- <h3 class="heading">Muuto</h3> -->
@@ -90,8 +110,100 @@ const setBg = ()=>{
 .content {
   position: relative;
   max-height: 100vh;
-  overflow: auto;
   z-index: 2;
+
+  &.-intro {
+    .upper {
+      clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);
+    }
+    .lower {
+      clip-path: polygon(0 0%, 100% 0%, 100% 0%, 0% 0%);
+    }
+  }
+}
+
+.page-margin {
+  margin-inline: 5vw;
+}
+
+
+.title {
+  position: relative;
+  width: 90vw;
+  height: 30vh;
+  margin: 5vh auto 0;
+  font-size: 60px;
+  font-weight: bold;
+  font-family: system-ui, serif;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, max-content));
+  place-content: center;
+  /* gap: .4em; */
+
+  .upper,
+  .lower {
+    display: grid;
+    grid-template-columns: subgrid;
+    grid-column: 1 / -1;
+  }
+
+  .upper {
+    display: grid;
+    grid-template-columns: subgrid;
+    grid-column: 2 / -1;
+    margin-left: auto;
+    margin-right: 0;
+    clip-path: polygon(0 0%, 100% 0%, 100% 100%, 0% 100%);
+    transition: clip-path 2.5s ease-out;
+  }
+
+  .lower {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    transition: clip-path 2.5s .5s ease-out;
+
+    div:first-child {
+      padding-right: .4em;
+    }
+  }
+
+  .spacer {
+    display: grid;
+    grid-template-columns: subgrid;
+    grid-column: 1 / -1;
+    height: 1px;
+    background-color: black;
+    transition: clip-path 1.5s .5s ease;
+  }
+}
+
+.outbound {
+  text-align: center;
+}
+.outbound-list {
+  margin-inline: auto;
+
+  li {
+    display: inline-block;
+    padding-right: 1em;
+  }
+}
+
+.tagline {
+  text-transform: uppercase;
+  font-weight: lighter;
+}
+.projects {
+  width: max-content;
+  margin-inline: auto;
+}
+.projects-list {
+  position: relative;
+  height: 7em;
+  overflow: scroll;
+}
+
+.no-link {
+  cursor: default;
 }
 
 .bgs {
